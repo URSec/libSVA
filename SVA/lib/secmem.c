b61e7ce418e4ea48741fc5846e64cc4681a0a50b
XiaowanDong <xdong@cs.rochester.edu>
2017-12-22 14:31:50 -0500

    Revert "Restored demand paging on Ghost Memory."
    This reverts commit 61ce60f2f96cd546523af363331f43c85b8ff20d.

--- SVA/lib/secmem.c
+++ SVA/lib/secmem.c
@@ -346,7 +346,7 @@ allocSecureMemory (void) {
    */
   unsigned char * vaddrStart = 0;
   struct SVAThread * threadp = cpup->currentThread;
-  if (threadp->secmemSize) {
+  if (threadp->secmemSize && !pgdef) {
     /*
      * Pretend to allocate more ghost memory (but let demand paging actually
      * map it in.
