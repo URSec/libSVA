e32b9a5f29fd7ee7ce369b562b2a86e1e5c85fa9
Colin Pronovost <colin.pron@live.com>
2019-10-02 15:13:31 -0400

    Add compiler.h
    Currently, this just sets the default visibility to hidden. This is
    necessary to avoid generating GOT relocations when building
    position-independent code.

---
+++ SVA/include/sva/compiler.h
@@ -0,0 +1,21 @@
+/*===- compiler.h - Compiler options and nonstandard keywords ---------------===
+ *
+ *                        Secure Virtual Architecture
+ *
+ * This file was developed by the LLVM research group and is distributed under
+ * the University of Illinois Open Source License. See LICENSE.TXT for details.
+ *
+ *===------------------------------------------------------------------------===
+ *
+ * This file sets compiler options (such as default visibility) and adds custom
+ * and semi-standard keywords (e.g. #define asm __asm__).
+ *
+ *===------------------------------------------------------------------------===
+ */
+
+#ifndef _SVA_COMPILER_H
+#define _SVA_COMPILER_H
+
+#pragma GCC visibility push(hidden)
+
+#endif /* _SVA_COMPILER_H */
