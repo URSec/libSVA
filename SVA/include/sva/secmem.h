abe9b8e07636854b560945fc075e77f7336c4765
Colin Pronovost <colin.pron@live.com>
2019-10-15 10:13:14 -0400

    Rename the `_ASM_CONST` macros and add a comment explaining their purpose

--- SVA/include/sva/secmem.h
+++ SVA/include/sva/secmem.h
@@ -18,41 +18,46 @@
 #ifndef _SVA_SECMEM_H
 #define _SVA_SECMEM_H
 
+/*
+ * We want these constants to have the `UL` suffix when used in C, but we also
+ * want them to be usable from assembly. These macros allow us to attach the
+ * suffix only when preprocessing C code.
+ */
 #ifdef __ASSEMBLER__
-#define _AC(X, Y) X
+#define _ASM_CONST(value, suffix) value
 #else
-#define __AC(X, Y) X ## Y
-#define _AC(X, Y) __AC(X, Y)
+#define __ASM_CONST(value, suffix) value ## suffix
+#define _ASM_CONST(value, suffix) __ASM_CONST(value, suffix)
 #endif
 
 #if !(defined(XEN) || defined(__XEN__))
 
 /* Start and end addresses of the secure memory (512GB) */
-#define SECMEMSTART _AC(0xfffffd0000000000, UL)
-#define SECMEMEND   _AC(0xfffffd8000000000, UL)
+#define SECMEMSTART _ASM_CONST(0xfffffd0000000000, UL)
+#define SECMEMEND   _ASM_CONST(0xfffffd8000000000, UL)
 
 /* Start and end addresses of the SVA direct mapping (512GB) */
-#define SVADMAPSTART _AC(0xfffffd8000000000, UL)
-#define SVADMAPEND   _AC(0xfffffe0000000000, UL)
+#define SVADMAPSTART _ASM_CONST(0xfffffd8000000000, UL)
+#define SVADMAPEND   _ASM_CONST(0xfffffe0000000000, UL)
 
 #else /* !XEN */
 
 /* Start and end addresses of the secure memory (1TB) */
-#define SECMEMSTART _AC(0xffff860000000000, UL)
-#define SECMEMEND   _AC(0xffff870000000000, UL)
+#define SECMEMSTART _ASM_CONST(0xffff860000000000, UL)
+#define SECMEMEND   _ASM_CONST(0xffff870000000000, UL)
 
 /* Start and end addresses of the SVA direct mapping (1TB) */
-#define SVADMAPSTART _AC(0xffff870000000000, UL)
-#define SVADMAPEND   _AC(0xffff880000000000, UL)
+#define SVADMAPSTART _ASM_CONST(0xffff870000000000, UL)
+#define SVADMAPEND   _ASM_CONST(0xffff880000000000, UL)
 
 /* Start address of PV guest memory (120TB) */
-#define GUESTSTART _AC(0xffff880000000000, UL)
+#define GUESTSTART _ASM_CONST(0xffff880000000000, UL)
 /* Guest memory ends at the end of the address space */
 
 #endif /* !XEN */
 
 /* Start and end addresses of user memory (128TB) */
-#define USERSTART _AC(0x0000000000000000, UL)
-#define USEREND   _AC(0x0000800000000000, UL)
+#define USERSTART _ASM_CONST(0x0000000000000000, UL)
+#define USEREND   _ASM_CONST(0x0000800000000000, UL)
 
 #endif /* _SVA_SECMEM_H */
