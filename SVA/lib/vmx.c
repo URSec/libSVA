08f857f46636b5cff2d2be9c55ffadc2174bc80b
Colin Pronovost <colin.pron@live.com>
2021-02-22 01:14:28 -0500

    Add XSS exiting bitmap to VMCS field whitelist

--- SVA/lib/vmx.c
+++ SVA/lib/vmx.c
@@ -3494,6 +3494,7 @@ readvmcs_checked(enum sva_vmcs_field field, uint64_t *data) {
     case VMCS_EOI_EXIT_BITMAP_2:
     case VMCS_EOI_EXIT_BITMAP_3:
     case VMCS_TPR_THRESHOLD:
+    case VMCS_XSS_EXITING_BITMAP:
       return readvmcs_unchecked(field, data);
 
     default:
@@ -4010,6 +4011,7 @@ writevmcs_checked(enum sva_vmcs_field field, uint64_t data) {
     case VMCS_EOI_EXIT_BITMAP_2:
     case VMCS_EOI_EXIT_BITMAP_3:
     case VMCS_TPR_THRESHOLD:
+    case VMCS_XSS_EXITING_BITMAP:
       return writevmcs_unchecked(field, data);
 
     default:
